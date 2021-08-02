8c02b9fcbcae99b80f912c4067834bebdcdd4a01
Nahid-Hasan551 <nahidhasannibir121370@gmail.com>
2021-08-02 11:46:48 +0600

    lab Task

---
+++ Lab-AJAX/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <?php $name ="sabbir";?>
+        <body>
+            <h1 align="center">My CV</h1>
+            <table align="center">
+                <tr> 
+                    <td><?php echo $name;?></td>
+                </tr>
+            </table>
+        </body>
+    </head>
+</html>
